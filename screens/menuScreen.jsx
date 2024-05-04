0c384fb517a6aba42ac4bb086755e8b6186f5776
Artyom-G <artyomgv05@gmail.com>
2024-05-03 22:19:42 -0400

    menu items mapped onto component

--- screens/menuScreen.jsx
+++ screens/menuScreen.jsx
@@ -5,41 +5,49 @@ import MenuItem from '../components/menuItem';
 const menu = require('../menu.json');
 
 export default function MenuScreen() {
-  return (
-    <View style={styles.container}>
-        <ScrollView>
-            {menu.restaurants.map((restaurant, indexRes) => {
-                return restaurant.categories.map((category, indexCat) => {
-                return category.items.map((item, indexItem) => {
-                    return (
-                    <TouchableOpacity key={indexItem} style={styles.menuItem}>
-                        <MenuItem
-                        name={item.name}
-                        restaurant={restaurant.restaurant}
-                        category={category.name}
-                        price={item.price}
-                        picture={item.picture}
-                        />
-                    </TouchableOpacity>
-                    );
-                });
-                });
-            })}
-        </ScrollView>
-    </View>
-  );
+
+    function filter(items, filterFunc){
+        
+    }
+
+    return (
+        <View style={styles.container}>
+            <ScrollView style={styles.scrollview}>
+                {menu.restaurants.map((restaurant, indexRes) => {
+                    return restaurant.categories.map((category, indexCat) => {
+                    return category.items.map((item, indexItem) => {
+                        return (
+                        <TouchableOpacity key={indexItem} style={styles.menuItem}>
+                            <MenuItem
+                            name={item.name}
+                            restaurant={restaurant.restaurant}
+                            category={category.name}
+                            price={item.price}
+                            picture={item.picture}
+                            />
+                        </TouchableOpacity>
+                        );
+                    });
+                    });
+                })}
+            </ScrollView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
   container: {
     marginTop: 100,
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+},
+scrollview:{
     gap: 10,
-    width: "100%"
-  },
+    width: "100%",
+    flex: 1,
+    
+},
   menuItem:{
     
   }
